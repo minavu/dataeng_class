9ef441ec0c89362fa70db2b3a79fb8636abaebf0
minavu <mina8@pdx.edu>
2022-04-15 16:37:34 -0700

    Submitting Week3

---
+++ Week3/producer_sample.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from confluent_kafka import Producer, KafkaError
+import json
+import ccloud_lib
+
+if __name__ == '__main__':
+
+    # Read arguments and configurations and initialize
+    args = ccloud_lib.parse_args()
+    config_file = args.config_file
+    topic = args.topic
+    conf = ccloud_lib.read_ccloud_config(config_file)
+
+    # Create Producer instance
+    producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    producer = Producer(producer_conf)
+
+    # Create topic if needed
+    ccloud_lib.create_topic(conf, topic)
+
+    delivered_records = 0
+
+    # Optional per-message on_delivery handler (triggered by poll() or flush())
+    # when a message has been successfully delivered or
+    # permanently failed delivery (after retries).
+    def acked(err, msg):
+        global delivered_records
+        """Delivery report handler called on
+        successful or failed delivery of message
+        """
+        if err is not None:
+            print("Failed to deliver message: {}".format(err))
+        else:
+            delivered_records += 1
+            print("Produced record to topic {} partition [{}] @ offset {}"
+                  .format(msg.topic(), msg.partition(), msg.offset()))
+
+    file = open('sample.json')
+    data = json.load(file)
+    for n in data:
+        record_key = "record"
+        record_value = json.dumps(n)
+        # print("Producing record: {}\t{}".format(record_key, record_value))
+        producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
+        # p.poll() serves delivery reports (on_delivery)
+        # from previous produce() calls.
+        producer.poll(0)
+
+    producer.flush()
+
+    print("{} messages were produced to topic {}!".format(delivered_records, topic))
